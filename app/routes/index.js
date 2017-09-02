70f7ee2ce5e0da20deb7304c0d2d0e865a88760c
@snowman <jimmoffitt@yahoo.com>
2017-09-02 09:53:32 -0600

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gnip 30-Day Search' });
+  res.render('index', { title: 'Twitter 30-Day Search' });
 });
 
 module.exports = router;
